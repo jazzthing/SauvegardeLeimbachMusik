1619b06c88aa00fcdf859460e7b942a1c16d9b49
jazzthing <thisisagata@gmail.com>
2017-07-23 12:17:11 +0200

    Agata's website

---
+++ domains/agatahusser.cf/public_html/index.php
@@ -0,0 +1,341 @@
+<!doctype html>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<title>Najlepszy Darmowy Hosting - Kreator Stron</title>
+<meta name="description" content="Najlepszy Darmowy Hosting WWW bez reklam o pojemności 100 MB i transferze 100 GB/mc. Za darmo dajemy Ci potężny DirectAdmin z którym ustawisz Email, FTP, MySQL, PHP. Darmowy kreator stron.">
+<meta name="keywords" content="najlepszy hosting,najlepszy darmowy hosting,darmowy hosting bez reklam,hosting stron,darmowy hosting www,darmowy kreator stron">
+<link href="http%3A%2F%2F24tm.pl%2Ffavicon.ico" rel="shortcut icon" type="image/x-icon">
+<style>
+html, body
+{
+   height: 100%;
+}
+div#space
+{
+   width: 1px;
+   height: 50%;
+   margin-bottom: -451px;
+   float:left
+}
+div#container
+{
+   width: 970px;
+   height: 902px;
+   margin: 0 auto;
+   position: relative;
+   clear: left;
+}
+body
+{
+   background-color: #8B0000;
+   color: #FFFFFF;
+   font-family: Arial;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+}
+</style>
+<link href="24tm.css" rel="stylesheet">
+<style>
+#wb_Text1 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_Text1 div
+{
+   text-align: center;
+}
+#Image7
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#wb_Text2 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_Text2 div
+{
+   text-align: center;
+}
+#wb_Text3 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_Text3 div
+{
+   text-align: center;
+}
+#wb_Text4 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_Text4 div
+{
+   text-align: center;
+}
+#Line1
+{
+   color: #333333;
+   background-color: #333333;
+   border-width: 0;
+   margin: 0;
+   padding: 0;
+}
+#wb_Text7 
+{
+   background-color: transparent;
+   background-image: none;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: center;
+}
+#wb_Text7 div
+{
+   text-align: center;
+}
+#wb_uid1
+{
+   color: #A9A9A9;
+   font-family: Arial;
+   font-size: 16px;
+}
+#wb_uid2
+{
+   color: #A9A9A9;
+   font-family: Arial;
+   font-size: 17px;
+}
+#wb_Text1
+{
+   position: absolute;
+   left: 396px;
+   top: 58px;
+   width: 453px;
+   height: 37px;
+   text-align: center;
+   z-index: 0;
+}
+#wb_uid3
+{
+   color: #696969;
+   font-family: Arial;
+   font-size: 13px;
+}
+#wb_Text2
+{
+   position: absolute;
+   left: 1px;
+   top: 266px;
+   width: 969px;
+   height: 34px;
+   text-align: center;
+   z-index: 2;
+}
+#wb_uid4
+{
+   color: #808080;
+   font-family: Arial;
+   font-size: 13px;
+}
+#wb_Text3
+{
+   position: absolute;
+   left: 1px;
+   top: 336px;
+   width: 969px;
+   height: 152px;
+   text-align: center;
+   z-index: 3;
+}
+#wb_uid5
+{
+   display: block;
+}
+#wb_Text4
+{
+   position: absolute;
+   left: 386px;
+   top: 885px;
+   width: 198px;
+   height: 16px;
+   text-align: center;
+   z-index: 4;
+}
+#wb_uid6
+{
+   display: inline-block;
+   width: 300px;
+   height: 250px;
+}
+#wb_uid7
+{
+   display: inline-block;
+   width: 300px;
+   height: 250px;
+}
+#Html1
+{
+   position: absolute;
+   left: 0px;
+   top: 135px;
+   width: 970px;
+   height: 90px;
+   z-index: 7;
+}
+#wb_uid8
+{
+   display: inline-block;
+   width: 300px;
+   height: 250px;
+}
+#Html2
+{
+   position: absolute;
+   left: 0px;
+   top: 526px;
+   width: 300px;
+   height: 250px;
+   z-index: 9;
+}
+#defaultHtml1
+{
+   position: absolute;
+   left: 669px;
+   top: 526px;
+   width: 300px;
+   height: 250px;
+   z-index: 8;
+}
+#wb_Text7
+{
+   position: absolute;
+   left: 92px;
+   top: 818px;
+   width: 786px;
+   height: 48px;
+   text-align: center;
+   z-index: 6;
+}
+#defaultHtml2
+{
+   position: absolute;
+   left: 335px;
+   top: 526px;
+   width: 300px;
+   height: 250px;
+   z-index: 10;
+}
+#wb_Image7
+{
+   position: absolute;
+   left: 0px;
+   top: 15px;
+   width: 284px;
+   height: 104px;
+   z-index: 1;
+}
+#Line1
+{
+   position: absolute;
+   left: 0px;
+   top: 805px;
+   width: 970px;
+   height: 1px;
+   z-index: 5;
+}
+#wb_uid0
+{
+   color: #00BFFF;
+   font-family: Arial;
+   font-size: 29px;
+}
+</style>
+</head>
+<body>
+<div id="space"><br></div>
+<div id="container">
+<div id="wb_Text1">
+<h1>Darmowy Hosting WWW</h1></div>
+<div id="wb_Image7">
+<a href="http://www.24tm.pl" target="_blank"><img src="http://www.24tm.pl/images/logo.png" id="Image7" alt=""></a></div>
+<div id="wb_Text2">
+<span id="wb_uid0"><strong>To jest strona domyślna założona na darmowym hostingu 24tm.PL.</strong></span></div>
+<div id="wb_Text3">
+<span id="wb_uid1"> </span><span id="wb_uid2"><strong>Zaloguj się do swojego <a href="http://www.24tm.pl/logowanie.php" class="style1">panelu sterowania</a><br>i skorzystaj z kreatora stron z szablonami, dzięki któremu zbudujesz profesjonalną witrynę w kilka minut lub<br>jeżeli już posiadasz stronę wyślij pliki na serwer pamiętając, żeby umieścić je w katalogu /public_html.<br> Potrzebujesz darmowej domeny zajrzyj <a href="http://www.24tm.pl/darmowe_domeny.php" class="style1">TU</a>.<br><br>Jeżeli potrzebujesz pomocy zaloguj się do panelu klienta i utwórz zgłoszenie.<br></a><br></strong></span></div>
+<div id="wb_Text4">
+<span id="wb_uid3"><a href="http://www.24tm.pl" class="style1">www.24tm.pl</a></span></div>
+<hr id="Line1">
+<div id="wb_Text7">
+<span id="wb_uid4"><a href="http://www.24tm.pl" class="style1">Strona Domowa</a> | <a href="http://www.24tm.pl/plan-hostingowy.php" class="style1">Plan Hostingowy</a> | <a href="http://www.24tm.pl/zaloz-bezplatne-konto.php" class="style1">Załóż Konto</a> | <a href="http://www.24tm.pl/darmowe_domeny.php" title="Darmowe domeny .TK / .Ml / .GA / .CF" class="style1">Darmowe Domeny</a> |<a href="http://www.24tm.pl/strefa-klienta.php" class="style1"> Strefa Klienta</a> |<a href="http://www.24tm.pl/pomoc-techniczna.php" class="style1"> Pomoc Techniczna</a> | <a href="http://www.24tm.pl/kontakt.php" class="style1">Kontakt</a><br><br>Darmowy&nbsp; hosting.bez zobowiązań</span></div>
+<div id="Html1">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 24tm czerwona -->
+<ins class="adsbygoogle"
+     id="wb_uid5"
+     data-ad-client="ca-pub-2348548573358151"
+     data-ad-slot="5671299170"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+<div id="defaultHtml1">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 24tm.pl300x250 -->
+<ins class="adsbygoogle"
+     id="wb_uid6"
+     data-ad-client="ca-pub-2348548573358151"
+     data-ad-slot="9137638378"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+<div id="Html2">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 24tm.pl300x250 -->
+<ins class="adsbygoogle"
+     id="wb_uid7"
+     data-ad-client="ca-pub-2348548573358151"
+     data-ad-slot="9137638378"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+<div id="defaultHtml2">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 24tm.pl300x250 -->
+<ins class="adsbygoogle"
+     id="wb_uid8"
+     data-ad-client="ca-pub-2348548573358151"
+     data-ad-slot="9137638378"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+</div>
+</body>
+</html>
